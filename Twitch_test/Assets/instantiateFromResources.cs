de3e515f658b6f2ed11bd6dde707a6d377da6ad8
Jiakai Chen <jchen154@ucsc.edu>
2019-04-21 21:56:26 -0700

    sample Scene

---
+++ Twitch_test/Assets/instantiateFromResources.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateFromResources : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject grenadePrefab = Resources.Load("Prefabs/Grenade") as GameObject;
+        GameObject grenade = GameObject.Instantiate(grenadePrefab, transform.position, Quaternion.Euler(90,0,0));
+        grenade.transform.position = new Vector3(5, 0 , 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
