e043b042c3c921fde7f13f1af839f1551d75c8b2
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-25 22:46:22 -0700

    li pao

---
+++ Twitch_test/Assets/StageController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+
+    [Header("CannonA1")]
+    public GameObject particle1;
+    public GameObject particle2;
+
+    [Header("CannonA2")]
+    public GameObject particle3;
+    public GameObject particle4;
+
+    [Header("CannonA3")]
+    public GameObject particle5;
+    public GameObject particle6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
