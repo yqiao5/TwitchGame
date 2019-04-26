374549c0bdccacabc4175137b992d5c9f2f0417c
wky2020 <wky2020@gmail.com>
2019-04-25 22:44:47 -0700

    尼尔子弹伤害

---
+++ Twitch_test/Assets/Scripts/DamagePlayerOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayerOnTouch : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private bool selfDestructAfterDamage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Character>().DecreaseHealth(damage);
+            if (selfDestructAfterDamage)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
