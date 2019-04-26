c230bbba0e915633807f55b209cc4f235b7397ba
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-25 20:07:09 -0700

    Game Scene

--- Twitch_test/Assets/Scripts/CameraControl.cs
+++ Twitch_test/Assets/Scripts/CameraControl.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public GameObject player;
-    Vector3 delta;
+    Vector3 delta=new Vector3(0,30,-19);
     // Start is called before the first frame update
     void Start()
     {
-        delta = transform.position- player.transform.position;
+        //delta = transform.position- player.transform.position;
     }
 
     // Update is called once per frame
