4a4c8141da96d3e1068aa23518a9954e19fd05aa
Kunyu Wang <wky2020@gmail.com>
2019-04-25 23:36:33 -0700

    1

--- Twitch_test/Assets/Scripts/CameraControl.cs
+++ Twitch_test/Assets/Scripts/CameraControl.cs
@@ -6,11 +6,6 @@ public class CameraControl : MonoBehaviour
 {
     public GameObject player;
     Vector3 delta=new Vector3(0,30,-19);
-    // Start is called before the first frame update
-    void Start()
-    {
-        //delta = transform.position- player.transform.position;
-    }
 
     // Update is called once per frame
     void Update()
