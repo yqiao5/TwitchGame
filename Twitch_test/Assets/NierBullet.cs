175c7ae5a77a3bc691173b238c720cdc6b9ea5b1
wky2020 <wky2020@gmail.com>
2019-04-25 21:44:29 -0700

    尼尔子弹自毁

--- Twitch_test/Assets/NierBullet.cs
+++ Twitch_test/Assets/NierBullet.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class NierBullet : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float lifeTime;
+    private float lifeTimer;
     // Start is called before the first frame update
     void Start()
     {
+        lifeTimer = lifeTime;
         GetComponent<Rigidbody>().velocity = transform.forward*speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer<=0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
