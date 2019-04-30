e8633da5714a599ecf3db744c62cb4be07f5e0c9
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-30 16:10:27 -0700

    BigWheelControl

---
+++ Twitch_test/Assets/BigWheelControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigWheelControl : MonoBehaviour
+{
+    float rotationSpeed= 360f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 50, 0);
+    }
+}
