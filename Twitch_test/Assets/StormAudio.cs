41d27ce4b3e036a1a423d4766e168da78481aa97
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-30 15:13:39 -0700

    Audio

---
+++ Twitch_test/Assets/StormAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormAudio : MonoBehaviour
+{
+    FMOD.Studio.EventInstance StormB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StormB = FMODUnity.RuntimeManager.CreateInstance("event:/StormB");
+        StormB.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
