2e4e20e4964b99b32c60547a770c835a7c21c26c
wky2020 <wky2020@gmail.com>
2019-04-29 19:50:17 -0700

    修改手榴弹伤害

--- Twitch_test/Assets/Scripts/Grenade.cs
+++ Twitch_test/Assets/Scripts/Grenade.cs
@@ -8,8 +8,23 @@ public class Grenade : MonoBehaviour
     [SerializeField] private int damage;
     [SerializeField] private float lifeTime;
     [SerializeField] private GameObject grenadeExplosionPrefab;
-    private bool onFloor;
 
+    private float lifeTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
