8eac0a74e367abb36501ae249f7647172d5d5641
wky2020 <wky2020@gmail.com>
2019-04-29 20:23:19 -0700

    风暴毁灭者伤害

--- Twitch_test/Assets/Scripts/DamagePlayerOnTouch.cs
+++ Twitch_test/Assets/Scripts/DamagePlayerOnTouch.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBullet : MonoBehaviour
+public class DamagePlayerOnTouch : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private bool selfDestructAfterDamage;
