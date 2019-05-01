4b901c76db5072a40ebe1d56e6238f5e27985fbe
wky2020 <wky2020@gmail.com>
2019-04-30 20:44:29 -0700

    hammer

---
+++ Twitch_test/Assets/ThorHammer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThorHammer : MonoBehaviour
+{
+    [SerializeField] private Transform tip;
+
+    public float rotateSpeed;
+
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //speed = defaultSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = Vector3.zero;
+        temp.z = rotateSpeed;
+        transform.Rotate(temp * Time.deltaTime);
+    }
+    void Rotate()
+    {
+        Vector3 temp = Vector3.zero;
+        temp.z = -30;
+        transform.eulerAngles = temp;
+    }
+}
