e70dd75bf5409f9d917e984b17dfca0491f288b8
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-30 17:54:53 -0700

    Gate

---
+++ Twitch_test/Assets/Scripts/BossGate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGate : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(other.transform.position.x > transform.position.x)
+            {
+                transform.GetComponent<Collider>().isTrigger = false;
+            }
+        }
+    }
+}
